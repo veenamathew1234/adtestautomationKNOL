3218d28741be8b8e98d8a5d6f657b365a99408d0
Shalini Chitroju <shalini.chitroju@knolskape.com>
2019-08-07 12:00:14 +0530

    mychanges

--- src/main/java/pageRepository/loginPage.java
+++ src/main/java/pageRepository/loginPage.java
@@ -61,8 +61,7 @@ public class loginPage extends StartUp{
 		Thread.sleep(1000);
 		cm.changeToNewUser();
 		Assert.assertEquals("Incorrect credentials",0,driver.findElements(By.xpath("//div[contains(@class,'error-box')]")).size());
-		cm.changeToNewUser();
-		
+				
 	}
 	
 
