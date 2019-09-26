284d850ea09d32548a341508cc913f5870b3e0a3
Veena Mathew <veena.mathew@knolskape.com>
2019-09-26 12:18:15 +0530

    wait in assignment

--- src/main/java/pageRepository/assignment.java
+++ src/main/java/pageRepository/assignment.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.Set;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.ObjectFactory;
 import utils.StartUp;
@@ -17,6 +19,7 @@ public class assignment extends StartUp {
 	List assignmentList;
 	WebElement e,e1;
 	Map<String,Object> CourseDataObj;
+	WebDriverWait wait = new WebDriverWait(driver,30);
 	
 	
 	public assignment(){
@@ -63,7 +66,8 @@ public class assignment extends StartUp {
 		
 		
 		e=driver.findElement(objmap.getLocator("lbl_assignmentname"));
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(objmap.getLocator("lbl_points")));
 		e1=driver.findElement(objmap.getLocator("lbl_points"));
 				
 		if(e.getText().equalsIgnoreCase(assignmentname) && e1.getText().equalsIgnoreCase(assignmentpoints)){
@@ -80,7 +84,8 @@ public class assignment extends StartUp {
 			
 			System.out.println("Inside text assignment submission");
 			driver.findElement(objmap.getLocator("txt_assignment")).sendKeys(assignmenttext);
-			Thread.sleep(2000);
+			//Thread.sleep(2000);
+			wait.until(ExpectedConditions.presenceOfElementLocated(objmap.getLocator("btn_submit")));
 			driver.findElement(objmap.getLocator("btn_submit")).click();
 			
 		}
@@ -90,14 +95,16 @@ public class assignment extends StartUp {
 			e.click();
 			Thread.sleep(2000);
 			e.sendKeys(System.getProperty("user.dir")+filetoupload);
-			Thread.sleep(1000);
+			//Thread.sleep(1000);
+			wait.until(ExpectedConditions.presenceOfElementLocated(objmap.getLocator("btn_submit")));
 			driver.findElement(objmap.getLocator("btn_submit")).click();
 		}
 		else if(assignmenttype.equalsIgnoreCase("Website URL")){
 			System.out.println("Inside website url assignment submission");
 			e=driver.findElement(objmap.getLocator("txt_assignmenturl"));
 			e.sendKeys(websiteurl);
-			Thread.sleep(2000);
+			//Thread.sleep(2000);
+			wait.until(ExpectedConditions.presenceOfElementLocated(objmap.getLocator("btn_submit")));
 			driver.findElement(objmap.getLocator("btn_submit")).click();
 			
 			
