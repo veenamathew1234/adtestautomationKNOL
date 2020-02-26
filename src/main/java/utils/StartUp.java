eb24e698f5ffd2b5572bf2358ca69de0f083945f
Ubuntu <azureuser@aktivlearn-automated-test-vm.b1etzextoieetb2uczjspmzsre.ix.internal.cloudapp.net>
2020-02-26 10:48:36 +0000

    as

--- src/main/java/utils/StartUp.java
+++ src/main/java/utils/StartUp.java
@@ -50,6 +50,7 @@ public class StartUp {
 				options.addArguments("-incognito");
 				options.addArguments("window-size=5000x5000");
 				options.addArguments("headless");
+                                options.addArguments("--no-sandbox");
 				options.setAcceptInsecureCerts(true);
 				options.addArguments("--allow-insecure-localhost");
 				options.addArguments("--ignore-certificate-errors");
