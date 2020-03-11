006c60e212d610ad103b9ac8db25d8fcfbe35d53
Ubuntu <azureuser@aktivlearn-automated-test-vm.b1etzextoieetb2uczjspmzsre.ix.internal.cloudapp.net>
2020-03-11 10:37:07 +0000

    corrected journey reference

--- src/main/java/utils/Hooks.java
+++ src/main/java/utils/Hooks.java
@@ -4,7 +4,7 @@ import cucumber.api.java.After;
 import pageRepository.journeyPage;
 
 public class Hooks {
-journeyPage1 jp=new journeyPage();
+journeyPage jp=new journeyPage();
 	
 	@After
     public void afterScenario(){
